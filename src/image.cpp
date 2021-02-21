81bdebadb952a317a5e89607eac839d53f702644
Tiago Ferreira <Tiago.Ferreira@criticaltechworks.com>
2021-02-21 22:28:37 +0000

    Dyslexia

--- src/image.cpp
+++ src/image.cpp
@@ -1,7 +1,7 @@
 #include "image.hpp"
 
 std::ostream& operator<<(std::ostream& o, const Image& image) {
-    std::cout << "weight: " << image.weight_ << std::endl;
+    std::cout << "width: " << image.width_ << std::endl;
     std::cout << "height: " << image.height_ << std::endl;
     std::cout << "color: " << image.color_ << std::endl;
     return o;
--- src/image.hpp
+++ src/image.hpp
@@ -8,13 +8,19 @@ class Image {
   public:
     using Color = Vec3f;
 
-    Image(int _weight, int _height, const Color& _color)
-      : weight_{_weight}, height_{_height}, color_{_color} {}
+    Image(int _width, int _height, const Color& _color)
+      : width_{_width}, height_{_height}, color_{_color} {}
+
+    int getWidth() const { return width_; }
+
+    int getHeight() const { return height_; }
+
+    Color getColor() const { return color_; }
 
     friend std::ostream& operator<<(std::ostream& o, const Image& image);
 
   private:
-    int weight_;
+    int width_;
     int height_;
     Color color_;
 };
