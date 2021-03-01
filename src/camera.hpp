d7a08b1fd021f75ba9684e75d766226fe9631ade
Tiago Ferreira <Tiago.Ferreira@criticaltechworks.com>
2021-03-01 21:41:47 +0000

    Added access methods to Camera

--- src/camera.hpp
+++ src/camera.hpp
@@ -9,6 +9,12 @@ class Camera {
     Camera(Transformation::Shared _transformation, float _distance, float _fov)
       : transformation_{_transformation}, distance_{_distance}, fov_{_fov} {}
 
+    auto getTransformation() const { return transformation_; }
+
+    auto getDistance() const { return distance_; }
+
+    auto getFov() const { return fov_; }
+
   private:
     Transformation::Shared transformation_;
     float distance_;
