011408019baaa746ec750cbdc1e449c273745402
Tiago Ferreira <Tiago.Ferreira@criticaltechworks.com>
2021-03-01 21:45:38 +0000

    Added ray printing

--- src/ray.cpp
+++ src/ray.cpp
@@ -1,1 +1,7 @@
 #include "ray.hpp"
+
+std::ostream& operator<<(std::ostream& out, const Ray& ray) {
+    out << "Origin   :" << ray.origin << std::endl;
+    out << "Direction:" << ray.direction << std::endl;
+    return out;
+}
--- src/ray.hpp
+++ src/ray.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
 #include "geometry.hpp"
+#include <iostream>
 
 class Ray {
   public:
-    Ray(const Vec3f& _origin, const Vec3f& _direction) : origin_(_origin), direction_(_direction) {}
-
-  private:
-    const Vec3f origin_;
-    const Vec3f direction_;
+    Ray(const Vec3f& _origin, const Vec3f& _direction) : origin(_origin), direction(_direction) {}
+    friend std::ostream& operator<<(std::ostream& out, const Ray& ray);
+    const Vec3f origin;
+    const Vec3f direction;
 };
