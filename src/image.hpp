3e19bb53d44777a7ee8dcbb2ce36339cd83d2183
Tiago Ferreira <Tiago.Ferreira@criticaltechworks.com>
2021-03-01 21:46:21 +0000

    Changed Image Color

--- src/image.hpp
+++ src/image.hpp
@@ -6,16 +6,16 @@
 
 class Image {
   public:
-    using Color = Vec3f;
+    using Color = Vec3<u_char>;
 
     Image(int _width, int _height, const Color& _color)
       : width_{_width}, height_{_height}, color_{_color} {}
 
-    int getWidth() const { return width_; }
+    auto getWidth() const { return width_; }
 
-    int getHeight() const { return height_; }
+    auto getHeight() const { return height_; }
 
-    Color getColor() const { return color_; }
+    auto getColor() const { return color_; }
 
     friend std::ostream& operator<<(std::ostream& o, const Image& image);
 
