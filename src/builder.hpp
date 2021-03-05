2f25b69463574fa4d39194af5e7b64065217692e
Tiago Ferreira <Tiago.Ferreira@criticaltechworks.com>
2021-03-05 16:17:27 +0000

    Improved Builder

--- src/builder.hpp
+++ src/builder.hpp
@@ -8,13 +8,9 @@ class Builder {
     using Shared = std::shared_ptr<OutputType>;
     using Unique = std::unique_ptr<OutputType>;
 
-    template <typename ShareType, typename... Args>
-    static std::enable_if_t<std::is_same_v<ShareType, Shared>, Shared> Build(Args&&... args) {
-        return std::make_shared<InputType>(std::forward<Args>(args)...);
-    }
 
-    template <typename ShareType, typename... Args>
-    static std::enable_if_t<std::is_same_v<ShareType, Unique>, Unique> Build(Args&&... args) {
+    template <typename Result = Shared, typename... Args>
+    static Result Build(Args&&... args) {
         return std::make_unique<InputType>(std::forward<Args>(args)...);
     }
 };
